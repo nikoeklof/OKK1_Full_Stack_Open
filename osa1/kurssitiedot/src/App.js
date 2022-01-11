a766eee9089247b81f38941e04b214458a0ad552
Niko / Nigey <dataaja.96@hotmail.com>
2022-01-11 10:43:25 +0200

    kurssitiedot valmis

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return(
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total total={course.parts}/>
     </div>
-  );
-}
+  )
 
-export default App;
+}
+const Header = (props) =>{
+  
+  return(
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+const Part = (props) =>{
+  console.log(props)
+  return(
+    <p>
+      {props.name}, Exercises: {props.exercises}
+    </p>
+  )
+}
+const Content = (props) =>{
+  console.log(props)
+  return(
+    <p>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </p>
+  )
+}
+const Total = (props) =>{
+  return(
+    <p>
+      Total number of exercises: {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}
+    </p>
+  )
+}
+export default App
