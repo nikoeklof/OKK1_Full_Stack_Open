3ca59c19585aaf1d01ea2dacd55c1c7818db303d
Niko / Nigey <dataaja.96@hotmail.com>
2022-01-14 10:14:44 +0200

    osa1 valmis

--- osa1/unicafe/anekdootit step2/App.js
+++ osa1/unicafe/anekdootit step2/App.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
-
+var votes = [0,0,0,0,0,0,0]
 const App = () => {
   // tallenna napit omaan tilaansa
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState([0,0,0,0,0,0,0])
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
 
   return (
     <div>
+      <h1>Anecdotes</h1>
+      <Button handleClick={() => setSelected(Math.round(Math.random() * 6))} text="next anecdote" />
+      <p>{anecdotes[selected]}</p>
+      <p>Votes: {votes[selected]}</p>
+      <Button handleClick={() => setVote(votes[selected] += 1)} text="vote" />
       <h1>Give Feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
@@ -28,6 +45,7 @@ const App = () => {
     </div>
   )
 }
+
 const Button = (props) => {
   return (
     <button onClick={props.handleClick}>{props.text}</button>
@@ -36,7 +54,7 @@ const Button = (props) => {
 
 
 const StatisticsLine = (props) => {
-  
+
   if (isNaN(props.amount)) {
     return (
       <tr>
