d06fedbd38f4f4118a8c1a582d1fe220b8a12702
Niko / Nigey <dataaja.96@hotmail.com>
2022-01-17 11:11:04 +0200

    osa2 valmis

--- osa2/kurssitiedot/kurssitiedot step9/App.js
+++ osa2/kurssitiedot/kurssitiedot step9/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Course from './Course'
+
 
 const App = () => {
   const courses = [
@@ -50,10 +50,54 @@ const App = () => {
 
   return (
     
-      <Course course={courses} />
+      <Courses course={courses} />
     
 
   )
 }
+const Courses = (props) => {
+  
+ const course = props.course.map((content) => <div key={content.id}>
+   <Header key={content.id} header={content.name}></Header>
+   <Part key={content.id + 1} parts={content.parts}></Part>
+   <Total total={content.parts}/>
+   </div>)
+  return(
+    <div>
+    {course}
+    </div>
+   
+    
+  )
+  
+}
 
+const Header = (props) => {
+  return (
+    <h1>
+      {props.header}
+    </h1>
+  )
+}
+const Part = (props) => {
+  console.log(props)
+  let parts = props.parts.map(part => <p key={part.id}>
+    {part.name}, Exercises: {part.exercises}
+  </p>)
+  
+  return (
+    <div>
+      {parts}
+    </div>
+    
+  )
+}
+
+const Total = (props) => {
+  const reducer = props.total.reduce((sum, order) => {
+    return sum + order.exercises
+  }, 0)
+
+  return <p>Total amount of exercises: {reducer}</p>
+}
 export default App
